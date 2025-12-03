47efc8e08732f380b2cca67b9851dad5aba04437
Gusmurai <gu7eva@yandex.ru>
2025-12-03 10:22:16 +0300

    Добавлена обработка ошибок и валидация ввода

--- PointRegion/Program.cs
+++ PointRegion/Program.cs
@@ -10,48 +10,57 @@ namespace PointRegion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите координату x:");
-            double x = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите координату y:");
-            double y = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите радиус r1:");
-            double r1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Введите радиус r2:");
-            double r2 = double.Parse(Console.ReadLine());
-
-            double rMin = Math.Min(r1, r2);
-            double rMax = Math.Max(r1, r2);
-
-            double distanceSquared = x * x + y * y;
-            double rMinSquared = rMin * rMin;
-            double rMaxSquared = rMax * rMax;
-
-            // Точность для сравнения (погрешность double)
-            double epsilon = 0.000001;
-
-            // 4. Проверяем условия
-            // Проверка: На границе
-            if (Math.Abs(distanceSquared - rMinSquared) < epsilon ||
-                Math.Abs(distanceSquared - rMaxSquared) < epsilon)
+            try
             {
-                Console.WriteLine("На границе");
+                Console.WriteLine("Введите координату x:");
+                double x = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите координату y:");
+                double y = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите радиус r1:");
+                double r1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите радиус r2:");
+                double r2 = double.Parse(Console.ReadLine());
+
+                // Проверка на корректность радиусов
+                if (r1 < 0 || r2 < 0)
+                {
+                    Console.WriteLine("Ошибка: Радиус не может быть отрицательным.");
+                    return;
+                }
+
+                double rMin = Math.Min(r1, r2);
+                double rMax = Math.Max(r1, r2);
+
+                double distanceSquared = x * x + y * y;
+                double rMinSquared = rMin * rMin;
+                double rMaxSquared = rMax * rMax;
+
+                double epsilon = 0.000001;
+
+                if (Math.Abs(distanceSquared - rMinSquared) < epsilon ||
+                    Math.Abs(distanceSquared - rMaxSquared) < epsilon)
+                {
+                    Console.WriteLine("На границе");
+                }
+                else if (distanceSquared > rMinSquared && distanceSquared < rMaxSquared)
+                {
+                    Console.WriteLine("Да");
+                }
+                else
+                {
+                    Console.WriteLine("Нет");
+                }
             }
-            // Проверка: Внутри области (кольца)
-            else if (distanceSquared > rMinSquared && distanceSquared < rMaxSquared)
+            catch (FormatException)
             {
-                Console.WriteLine("Да");
-            }
-            // Иначе: Вне области
-            else
-            {
-                Console.WriteLine("Нет");
+                Console.WriteLine("Ошибка: Пожалуйста, вводите только числа.");
             }
 
             Console.ReadLine();
         }
     }
-    }
-
+}
+    
