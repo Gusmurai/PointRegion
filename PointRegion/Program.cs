d197e0fc39fbf967c5343ab947efafc1fe6cf9c2
Gusmurai <gu7eva@yandex.ru>
2025-12-03 09:40:10 +0300

    Структура и чтение данных

--- PointRegion/Program.cs
+++ PointRegion/Program.cs
@@ -10,6 +10,21 @@ namespace PointRegion
     {
         static void Main(string[] args)
         {
+            // 1. Ввод данных
+            Console.WriteLine("Введите координату x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату y:");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите радиус r1:");
+            double r1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите радиус r2:");
+            double r2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Данные получены. Нажмите Enter...");
+            Console.ReadLine();
         }
     }
 }
